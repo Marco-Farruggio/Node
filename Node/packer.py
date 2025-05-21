cbf0f045759c57251c8b0a07f7c4c983bf8e7682
Marco Farruggio <marcofarruggiopersonal@gmail.com>
2025-05-21 17:19:30 +0100

    Delete Node directory

--- Node/packer.py
+++
@@ -1,7 +0,0 @@
-import struct
-from typing import Union
-
-def pack(sender_id: Union[int, str], message: str, encoding: str) -> bytes:
-    """Sender ID (32Bit)"""
-            
-    return struct.pack("!I", sender_id) + message.encode(encoding)
